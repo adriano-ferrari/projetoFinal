69d5d3de885c8a3113173a34ae5966e15ca16840
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-03-29 18:54:30 -0300

    169. Movimentos Rotativos - Calculos de valores

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Marca, Veiculo, Pessoa, Parametro, MovRotativo
 
 
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
+
+
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametro)
-admin.site.register(MovRotativo)
+admin.site.register(MovRotativo, MovRotativoAdmin)
