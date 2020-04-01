2c2d3e7de075af7afa8f1f94e2adb884547d13f7
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-04-01 16:35:38 -0300

    175. Listagem veiculos e MovRotativo

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import home, lista_pessoas
+from .views import home, lista_pessoas, lista_veiculos, lista_movrotativos
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
+    path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
+    path('mov-rot-list/', lista_movrotativos, name='core_lista_movrotativos'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import Pessoa, Veiculo, MovRotativo
 
 
 
@@ -11,3 +11,14 @@ def home(request):
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+
+def lista_movrotativos(request):
+    mov_rot = MovRotativo.objects.all()
+    return render(request, 'core/lista_movrotativos.html',
+                  {'mov_rot': mov_rot})
