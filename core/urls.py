ab0eb2dfcc527272996fbb0b86d5fd32a43080c4
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-04-09 21:59:33 -0300

    187. Delete Pessoa

--- core/urls.py
+++ core/urls.py
@@ -4,29 +4,30 @@ from .views import (home, lista_pessoas, lista_veiculos, lista_movrotativos,
                     veiculo_novo, movrotativo_novo, mensalista_novo,
                     movmensalista_novo, pessoa_update, veiculo_update,
                     movrotativo_update, mensalista_update,
-                    movmensalista_update)
+                    movmensalista_update, pessoa_delete,)
 
 urlpatterns = [
     path('', home, name='core_home'),
 
     path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
     path('pessoa-nova/', pessoa_nova, name='core_pessoa_nova'),
-    path('pessoa-update/<int:id>/$', pessoa_update, name='core_pessoa_update'),
+    path('pessoa-update/<int:id>/', pessoa_update, name='core_pessoa_update'),
+    path('pessoa-delete/<int:id>/', pessoa_delete, name='core_pessoa_delete'),
 
     path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
     path('veiculo-novo/', veiculo_novo, name='core_veiculo_novo'),
-    path('veiculo-update/<int:id>/$', veiculo_update, name='core_veiculo_update'),
+    path('veiculo-update/<int:id>/', veiculo_update, name='core_veiculo_update'),
 
     path('mov-rot/', lista_movrotativos, name='core_lista_movrotativos'),
     path('mov-rot-novo/', movrotativo_novo, name='core_movrotativo_novo'),
-    path('mov-rot-update/<int:id>/$', movrotativo_update,
+    path('mov-rot-update/<int:id>/', movrotativo_update,
          name='core_movrotativo_update'),
 
     path('mensalistas/', lista_mensalistas, name='core_lista_mensalistas'),
     path('mensalista-novo/', mensalista_novo, name='core_mensalista_novo'),
-    path('mensalista-update/<int:id>/$', mensalista_update, name='core_mensalista_update'),
+    path('mensalista-update/<int:id>/', mensalista_update, name='core_mensalista_update'),
 
     path('mov-mensal/', lista_movmensalista, name='core_lista_movmensalistas'),
     path('mov-mensal-novo/', movmensalista_novo, name='core_movmensalista_novo'),
-    path('mov-mensal-update/<int:id>/$', movmensalista_update, name='core_movmensalista_update'),
+    path('mov-mensal-update/<int:id>/', movmensalista_update, name='core_movmensalista_update'),
 ]
